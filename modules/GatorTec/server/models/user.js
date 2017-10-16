3ea6538ee310870fc464d5ca468ab93421bf225d
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-10-16 16:07:22 -0400

    Modified user model to work with the passport.js authentication module

--- modules/GatorTec/server/models/user.js
+++ modules/GatorTec/server/models/user.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 // Model for the user
 let user = new Schema({
 
-  userEmail: String,
+  username: String,
   userPassword: String,
   userRole: String
 
 });
+
+user.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('user', user);
