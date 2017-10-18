89ca85dbded5f6b9b1685514c9440d2af634cc37
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-10-18 15:32:01 -0400

    Added unblacklist endpoint and comments indicating what needs to be done

--- modules/GatorTec/server/routes/repairOrderRoutes.js
+++ modules/GatorTec/server/routes/repairOrderRoutes.js
@@ -3,17 +3,23 @@ const express = require('express');
 const router = express.Router();
 
 // Defining routes for the different kinds of request made for a repairOrder
-router.route('/add')
+router.route('/add')            // Create middleware for getting largest sroID and incrementing then assign it to the new repair order
   .post(repairOrder.add);
 
 router.route('/getAllRepairOrders')
   .get(repairOrder.getAllRepairOrders);
 
-router.route('/edit')
-  .put(repairOrder.edit);
+router.route('/getAllRepairOrdersByEmail')        // Get the GET request to accept parameters; Specifically username/email
+  .get(repairOrder.getAllRepairOrdersByEmail);
 
-router.route('/blacklist')
+  router.route('/blacklist')
   .put(repairOrder.blacklist);
+
+  router.route('/unblacklist')
+  .put(repairOrder.unblacklist);
+
+// router.route('/edit')
+//   .put(repairOrder.edit);
 
 router.route('/delete')
   .delete(repairOrder.delete);
