4b6199a9707b291eb01fe7de504a64ff6d77f3a3
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-10-18 15:11:37 -0400

    Deleted registrationRouter after moving registration function to endpoint user/add

--- modules/GatorTec/server/routes/registrationRouter.js
+++
@@ -1,9 +0,0 @@
-const registration = require('../controllers/registrationController.js');
-const express = require('express');
-const router = express.Router();
-
-// Defining routes for the different kinds of request made for a customer
-router.route('/')
-  .post(registration.register);
-
-module.exports = router;
