bd4fe2b27d320abd75b2af362c186efd9ec62c37
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-10-18 15:32:46 -0400

    Added comments indicating what needs to be done

--- modules/GatorTec/server/routes/userRoutes.js
+++ modules/GatorTec/server/routes/userRoutes.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 // Defining routes for the different kinds of request made for a user
-router.route('/add')
+router.route('/add')              // Create middleware that will check that the username/email is not already taken
   .post(user.add);
 
 router.route('/getAllusers')
   .get(user.getAllUsers);
 
-router.route('/edit')
-  .put(user.edit);
+// router.route('/edit')
+//   .put(user.edit);
 
 router.route('/delete')
   .delete(user.delete);
