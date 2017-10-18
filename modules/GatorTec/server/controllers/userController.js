6bcf5e3c512347eff6e01eb2c25ee55a36dce7e3
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-10-18 15:25:40 -0400

    Moved registration function into user/add endpoint

--- modules/GatorTec/server/controllers/userController.js
+++ modules/GatorTec/server/controllers/userController.js
@@ -1,17 +1,25 @@
 const mongoose = require('mongoose');
 const user = require('../models/user.js');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 // Add an user
 exports.add = function(req, res){
 
-  let user = req.body;
+  let username = req.body.username;
+  let userRole = req.body.userRole;
+  let userPassword = req.body.userPassword;
 
-  user.create(user, function(err, user){
-    if(err){
-      res.status(403).send('Bad Request');
-    }
+  user.register(new user({ username : username, userRole: userRole }), userPassword, function(err, user) {
 
-    res.status(200).send(user);
+      if (err) {
+          return res.status(403).send("Bad Request");
+      }
+
+      passport.authenticate('local')(req, res, function () {
+          res.redirect('/');
+      });
+
   });
 
 };
@@ -20,6 +28,7 @@ exports.add = function(req, res){
 exports.getAllUsers = function(req, res){
 
   user.find({}, function(err, users){
+
     if(err){
       res.status(500).send('Internal Server Error');
     }
@@ -29,27 +38,29 @@ exports.getAllUsers = function(req, res){
 
 };
 
-// Edit an user by email
-exports.edit = function(req, res){
+// Edit an user by username/email
+// exports.edit = function(req, res){
+//
+//   let username = req.body.username;
+//   let userRole = req.body.userRole;
+//
+//   user.findOneAndUpdate({ username: username }, { userRole: userRole }, { new: true }, function(err, user){
+//     if(err){
+//       res.status(403).send('Bad Request');
+//     }
+//
+//     res.status(200).send(user);
+//   });
+//
+// };
 
-  let userEmail = req.body.userEmail;
-
-  user.findOneAndUpdate({ userEmail: userEmail }, { userType: "admin" }, { new: true }, function(err, user){
-    if(err){
-      res.status(403).send('Bad Request');
-    }
-
-    res.status(200).send(user);
-  });
-
-};
-
-// Delete an user by email
+// Delete an user by username/email
 exports.delete = function(req, res){
 
-  let userEmail = req.body.userEmail;
+  let username = req.body.username;
 
-  user.findOneAndRemove({ userEmail: userEmail }, function(err, user){
+  user.findOneAndRemove({ username: username }, function(err, user){
+
     if(err){
       res.status(500).send('Internal Server Error');
     }
