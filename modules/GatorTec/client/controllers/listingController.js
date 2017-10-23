b846798ca1d3d55bdb611453fcb10c3de069a942
Macy Weston <macyweston@ufl.edu>
2017-10-23 14:15:11 -0400

    Device scroll bar implement & styling changes

--- modules/GatorTec/client/controllers/listingController.js
+++ modules/GatorTec/client/controllers/listingController.js
@@ -1,24 +1,34 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
-  function($scope, Listings) {
-    /* Get all the listings, then bind it to the scope */
-    Listings.getAll().then(function(response) {
-      $scope.listings = response.data;
-    }, function(error) {
-      console.log('Unable to retrieve listings:', error);
-    }
-  );
+function($scope, Listings) {
+  /* Get all the listings, then bind it to the scope */
+  Listings.getAll().then(function(response) {
+    $scope.listings = response.data;
+    $scope.currentDevice = response.data[0];
+  }, function(error) {
+    console.log('Unable to retrieve listings:', error);
+  });
 
-    /*$scope.addListing = function() {
-      $scope.listings.push($scope.newListing);
-      $scope.newListing = {};
-    };
+  $scope.switchDevice = function(listing) {
+    $scope.currentDevice = listing;
+  };
 
-    $scope.deleteListing = function(index) {
-      $scope.listings.splice($scope.listings.indexOf(index),1);
-    };
+  $scope.scrollToTop = function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
 
-    $scope.showDetails = function(index) {
-      $scope.detailedInfo = {'name':index.name, 'address':index.address, 'code':index.code}
-    };*/
-  }
+  /*
+  $scope.addListing = function() {
+    $scope.listings.push($scope.newListing);
+    $scope.newListing = {};
+  };
+
+  $scope.deleteListing = function(index) {
+    $scope.listings.splice($scope.listings.indexOf(index),1);
+  };
+
+  $scope.showDetails = function(index) {
+    $scope.detailedInfo = {'name':index.name, 'address':index.address, 'code':index.code}
+  };*/
+}
 ]);
