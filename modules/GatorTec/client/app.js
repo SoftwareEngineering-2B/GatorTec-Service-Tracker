74249714f6c10a72db060cbb005a10c1ec494b33
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-10-26 20:11:19 -0400

    Added additional routing to the other pages

--- modules/GatorTec/client/app.js
+++ modules/GatorTec/client/app.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  angular.module('app', ['login', 'customer', 'ui.router'])
+  angular.module('app', ['login', 'customer', 'technician', 'admin', 'ui.router'])
     .config(function($stateProvider, $urlRouterProvider){
 
       $urlRouterProvider.otherwise('/login');
@@ -16,17 +16,33 @@
           url: '/customer',
           templateUrl: './views/customerView.html',
           controller:'customerController'
+        })
+        .state('technician', {
+          url: '/technician',
+          templateUrl: './views/technicianView.html',
+          controller: 'technicianController'
+        })
+        .state('admin', {
+          url: '/admin',
+          abstract: true,
+          template: '<ui-view/>'
+        })
+        .state('admin.customers', {
+          url: '/customers',
+          templateUrl: './views/adminView.customers.html',
+          controller: 'adminController'
+        })
+        .state('admin.users', {
+          url: '/users',
+          templateUrl: './views/adminView.users.html',
+          controller: 'adminController'
+        })
+        .state('admin.database', {
+          url: '/database',
+          templateUrl: './views/adminView.database.html',
+          controller: 'adminController'
         });
-        // .state('technician', {
-        //   url: 'technician',
-        //   templateUrl: './views/technicianView.html',
-        //   controller: 'technicianController'
-        // })
-        // .state('admin', {
-        //   url: '/admin',
-        //   templateUrl: './views/adminView.html',
-        //   controller: 'adminController'
-        // });
+
 
     });
 
