8f3ef3a74dbf5cc2a97d9d0ea5ad49141e48b5f1
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-10-30 11:51:59 -0400

    Injected newly created module httpService

--- modules/GatorTec/client/app.js
+++ modules/GatorTec/client/app.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  angular.module('app', ['login', 'customer', 'technician', 'admin', 'ui.router'])
+  angular.module('app', ['login', 'customer', 'technician', 'admin', 'httpService', 'ui.router'])
     .config(function($stateProvider, $urlRouterProvider){
 
       $urlRouterProvider.otherwise('/login');
