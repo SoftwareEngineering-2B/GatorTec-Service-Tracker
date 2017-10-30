bc8f72aa00555b23e66604903407472c41c90146
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-10-30 11:44:46 -0400

    Abstracted out login functionality to a service I created called httpAPI

--- modules/GatorTec/client/controllers/loginController.js
+++ modules/GatorTec/client/controllers/loginController.js
@@ -2,36 +2,12 @@
   'use strict';
 
   angular.module('login', [])
-  .controller('loginController', [ '$scope', '$http', '$state', function($scope, $http, $state) {
+  .controller('loginController', ['$scope', 'httpAPI', function($scope, httpAPI) {
     $scope.field1 = "";
     $scope.field2 = "";
 
     $scope.login = function(){
-
-      let body = {
-        "username": $scope.field1,
-        "userPassword": $scope.field2
-      };
-
-      $http({
-        method: 'POST',
-        url: 'http://localhost:8080/user/login',
-        data: body
-      })
-      .then(function successCallback(response){
-        if(response.data == 'customer'){
-          $state.go('customer');
-        }
-        else if(response.data == 'technician'){
-          $state.go('technician');
-        }
-        else if(response.data == 'admin'){
-          $state.go('admin.users');
-        }
-      }, function errorCallback(response){
-        console.log('failure');
-      });
-
+      httpAPI.login($scope.field1, $scope.field2);
     };
 
   }]);
