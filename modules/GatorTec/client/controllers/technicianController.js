1674f027dccc536dad6aae3a78557fb590d61396
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-10-30 23:59:17 -0400

    cleaned up controller and removed console logs

--- modules/GatorTec/client/controllers/technicianController.js
+++ modules/GatorTec/client/controllers/technicianController.js
@@ -3,29 +3,20 @@
 
   angular.module('technician', [])
   .controller('technicianController', ['$scope', 'httpAPI', function($scope, httpAPI) {
-     $scope.customers =[];
+
+     $scope.customers = [];
 
      httpAPI.getAllRepairOrders().then(function(response){
        $scope.customers = response;
-      //  console.log($scope.customers);
      });
 
      $scope.blacklist = function(sroID){
-       console.log(sroID);
       //  httpAPI.blacklistRepairOrder(sroID);
      };
 
      $scope.unblacklist = function(sroID){
-       console.log(sroID);
       //  httpAPI.unblacklistRepairOrder(sroID);
      };
-
-     console.log($scope.customers);
-
-
-
-    // httpAPI.blacklistRepairOrder();
-    // httpAPI.unblacklistRepairOrder();
 
   }]);
 })();
