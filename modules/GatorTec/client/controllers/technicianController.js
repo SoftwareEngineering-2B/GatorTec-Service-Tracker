8ef85ef892bc5a60df76ab9b6648c7e8474d50b6
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-10-31 07:55:04 -0400

    Added blacklist/unblacklist and sorting functionality

--- modules/GatorTec/client/controllers/technicianController.js
+++ modules/GatorTec/client/controllers/technicianController.js
@@ -8,14 +8,29 @@
 
      httpAPI.getAllRepairOrders().then(function(response){
        $scope.customers = response;
+       $scope.customers.sort(function(a,b){
+         return a.sroID - b.sroID;
+       });
      });
 
      $scope.blacklist = function(sroID){
-      //  httpAPI.blacklistRepairOrder(sroID);
+       httpAPI.blacklistRepairOrder(sroID).then(function(response){
+         for(let i in $scope.customers){
+           if($scope.customers[i].sroID == sroID){
+             $scope.customers[i].blacklist = true;
+           }
+         }
+       });
      };
 
      $scope.unblacklist = function(sroID){
-      //  httpAPI.unblacklistRepairOrder(sroID);
+       httpAPI.unblacklistRepairOrder(sroID).then(function(response){
+         for(let i in $scope.customers){
+           if($scope.customers[i].sroID == sroID){
+             $scope.customers[i].blacklist = false;
+           }
+         }
+       });
      };
 
   }]);
