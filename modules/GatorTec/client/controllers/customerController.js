379ab3ac86b8690792e54a974d6bb10b1912e036
Macy Weston <macyweston@ufl.edu>
2017-11-15 13:44:12 -0500

    customer view functionality implemented

--- modules/GatorTec/client/controllers/customerController.js
+++ modules/GatorTec/client/controllers/customerController.js
@@ -3,15 +3,71 @@
 
   angular.module('customer', [])
   .controller('customerController', ['$scope', 'httpAPI', function($scope, httpAPI) {
-
+    
     httpAPI.getAllRepairOrdersByEmail().then(function(response){
       console.log(response);
       $scope.repairOrders = response;
+      $scope.currentDevice = response[0];
+      $scope.progress1 = "100";
+      if(response[0].status == "Device Received"){
+        $scope.progress2 = "0";
+        $scope.progress3 = "0";
+      }
+      else if(response[0].status == "On Bench"){
+        $scope.progress2 = "50";
+        $scope.progress3 = "0";
+      }
+      else if(response[0].status == "Waiting for Customer"){
+        $scope.progress2 = "50";
+        $scope.progress3 = "0";
+        // ADD POP UP
+      }
+      else if(response[0].status == "Parts Ordered"){
+        $scope.progress2 = "100";
+        $scope.progress3 = "0";
+      }
+      else if(response[0].status == "Ready"){
+        $scope.progress2 = "100";
+        $scope.progress3 = "75";
+      }
+      else if(response[0].status == "Invoiced"){
+        $scope.progress2 = "100";
+        $scope.progress3 = "100";
+      }
       $scope.repairOrders.sort(function(a,b){
         return a.sroID - b.sroID;
       });
       console.log($scope.repairOrders);
     });
+
+    $scope.switchDevice = function(repairOrder) {
+      $scope.currentDevice = repairOrder;
+      if(repairOrder.status == "Device Received"){
+        $scope.progress2 = "0";
+        $scope.progress3 = "0";
+      }
+      else if(repairOrder.status == "On Bench"){
+        $scope.progress2 = "50";
+        $scope.progress3 = "0";
+      }
+      else if(repairOrder.status == "Waiting for Customer"){
+        $scope.progress2 = "50";
+        $scope.progress3 = "0";
+        // ADD POP UP
+      }
+      else if(repairOrder.status == "Parts Ordered"){
+        $scope.progress2 = "100";
+        $scope.progress3 = "0";
+      }
+      else if(repairOrder.status == "Ready"){
+        $scope.progress2 = "100";
+        $scope.progress3 = "75";
+      }
+      else if(repairOrder.status == "Invoiced"){
+        $scope.progress2 = "100";
+        $scope.progress3 = "100";
+      }
+    }
 
     $scope.logout = function(){
       httpAPI.logout().then(function(response){
--- modules/GatorTec/server/config/config.js
+++ modules/GatorTec/server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: 'mongodb://meanjs:meanjs@ds139844.mlab.com:39844/meanjs-swe',
+  db: 'mongodb://gatortec-user:/q8E7txRm=AurPZV@ds159235.mlab.com:59235/gatortec-service-tracker',
 }
 //
 // module.exports = {
