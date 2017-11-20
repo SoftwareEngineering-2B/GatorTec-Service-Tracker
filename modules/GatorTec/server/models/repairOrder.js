082753c4cdae2b31ed58ae5ce319c33e8e482a78
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-11-20 10:16:31 -0500

    Added required property to fields in repairOrder model

--- modules/GatorTec/server/models/repairOrder.js
+++ modules/GatorTec/server/models/repairOrder.js
@@ -4,15 +4,16 @@ const Schema = mongoose.Schema;
 // Model for the repairOrder
 let repairOrder = new Schema ({
 
-  sroID: String,
-  customerName: String,
+  sroID: { type: String, required: true },
+  customerName: { type: String, required: true },
   customerPhoneNumber: String,
-  customerEmail: String,
-  deviceType: String,
-  serialNumber: String,
-  status: String,
-  dateCreated: String,
-  dateModified: String,
+  customerEmail: { type: String, required: true },
+  deviceType: { type: String, required: true },
+  serialNumber: { type: String, required: true },
+  warrantyStatus: String,
+  status: { type: String, required: true },
+  dateCreated: { type: String, required: true },
+  dateModified: { type: String, required:true },
   problemDescription: String,
   workPerformed: String,
   blacklist: Boolean
