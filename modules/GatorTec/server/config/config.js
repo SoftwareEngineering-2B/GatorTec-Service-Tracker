69b887f93d47856374facf21148eb4bdd71bb64e
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-11-28 00:14:44 -0500

    Modified db

--- modules/GatorTec/server/config/config.js
+++ modules/GatorTec/server/config/config.js
@@ -1,11 +1,7 @@
-// module.exports = {
-//   db: 'mongodb://gatortec-user:/q8E7txRm=AurPZV@ds159235.mlab.com:59235/gatortec-service-tracker',
-// }
-
-// module.exports = {
-//   db: 'mongodb://meanjs:meanjs@ds139844.mlab.com:39844/meanjs-swe',
-// }
-
 module.exports = {
-  db: 'mongodb://testing:testing@ds139342.mlab.com:39342/testing-gatortec'
+  db: 'mongodb://gatortec-user:/q8E7txRm=AurPZV@ds159235.mlab.com:59235/gatortec-service-tracker',
 }
+
+// module.exports = {
+//   db: 'mongodb://testing:testing@ds139342.mlab.com:39342/testing-gatortec'
+// }
