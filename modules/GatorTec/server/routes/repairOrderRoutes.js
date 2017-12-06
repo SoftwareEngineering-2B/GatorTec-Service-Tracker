6a30ec0cd2f2e90f96b57fa1fc125eac8de450be
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-12-05 21:53:41 -0500

    removed repairOrder.createUserFromSRO from the /add route

--- modules/GatorTec/server/routes/repairOrderRoutes.js
+++ modules/GatorTec/server/routes/repairOrderRoutes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const authenticate = require('../config/authenticate.js');
 
 // Defining routes for the different kinds of request made for a repairOrder
-router.route('/add')            // Create middleware for getting largest sroID and incrementing then assign it to the new repair order
-  .post(authenticate.isAdmin, repairOrder.add, repairOrder.createUserFromSRO);
+router.route('/add')
+  .post(authenticate.isAdmin, repairOrder.add);
 
 router.route('/getAllRepairOrders')
   .get(authenticate.isTechnicianOrAdmin, repairOrder.getAllRepairOrders);
